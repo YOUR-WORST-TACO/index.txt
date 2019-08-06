71688e781b77f54d86ef5ffce25e94f2f03b790b
your-worst-taco <stephentafoya@outlook.com>
2019-08-06 12:27:54 -0600

    Migrated to gulp from grunt

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const del = require('del');
+const rename = require('gulp-rename');
+
+sass.compiler = require('node-sass');
+
+gulp.task('sass', () => {
+    gulp.src('Styles/**/*.scss')
+        .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+        .pipe(gulp.dest('wwwroot/css/'));
+    return gulp.src('Styles/**/*.scss')
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(rename('main.min.css'))
+        .pipe(gulp.dest('wwwroot/css/'));
+});
+
+gulp.task('sass:clean', () => {
+    return del([
+        'css/main.css',
+        'css/main.min.css',
+    ]);
+});
+
+gulp.task('sass:watch', () => {
+    gulp.watch('Styles/**/*.scss', gulp.series('sass'));
+});
